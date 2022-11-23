30f8dd970f26ca446925bbd447989d78bde99563
Hasan-Inam <117076264+Hasan-Inam@users.noreply.github.com>
2022-11-23 23:01:23 +0300

    Update and rename src/main/java/Pull01.java to kaynak/ana/java/Pull01.java

--- kaynak/ana/java/Pull01.java
+++ kaynak/ana/java/Pull01.java
@@ -1,5 +1,6 @@
 public class Pull01 {
     public static void main(String[] args) {
-        System.out.println("Hello world..");
+        System.out.println("Hello world.."); 
+        //
     }
 }
